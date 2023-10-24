2c973bd8563037db1e2ea84060f2530688b048d7
IgorShchegel <mitosss777@gmail.com>
2023-10-24 14:36:37 +0300

    correction

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth(content, padding, border) {
     const totalPading = Number.parseFloat(padding) * 2;
     const totalBorder = Number.parseFloat(border) * 2;
-    return `${ Number.parseFloat(content) + totalPading + totalBorder }`
+    return  Number.parseFloat(content) + totalPading + totalBorder ;
 };
 
 
@@ -9,3 +9,5 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
+  
+
